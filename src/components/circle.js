24aa2e5bbad760676b89b9c49961fbc458e7b0f2
PRADHUN-PRINCE <pradhunprince22@gmail.com>
2023-03-21 02:12:42 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,19 @@
 // import using require
 
 // declare class
+// import using require
+var Shape = require('./shape.js');
+// declare class
+class Circle extends Shape {
+     constructor(color) {
+          super();
+     }
+     calculateArea() {
 
+     }
+}
+
+// export class using module.exports
+module.exports = Circle;
 
 // export class using module.exports
